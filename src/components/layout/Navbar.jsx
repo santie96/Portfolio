86ce3dbfd14f77f746279e21f1cccef08da4a735
santie96 <santi96emma@gmail.com>
2026-06-30 21:17:39 +0200

    fix/navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,27 +1,29 @@
-import { IoCodeSlashSharp } from "react-icons/io5";
+import { HiCode } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
+import logoPortfolio from "../../assets/img/logo-portolio.png";
 
 function Navbar() {
     return (
         <>
-            <section className=" bg-pink-600 w-full py-4 flex items-center justify-around text-center">
-                <div className="flex items-center gap-2 text-3xl">
-                    <IoCodeSlashSharp />
-                    <h2>Santi Andrea Emma</h2>
+            <section className="bg-bg-primary w-full flex justify-center py-8 border-b-2 border-text-primary">
+                <div className="flex items-center justify-between text-center w-4/5">
+                    <div className="flex items-center gap-4">
+                        <img className="w-18" src={logoPortfolio} alt="Logo portfolio" />
+                    </div>
+                    <nav>
+                        <ul className="flex gap-4 font-text text-1xl font-medium text-text-primary">
+                            <li>
+                                <NavLink className="" to="/progetti">Progetti</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about">Chi sono</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contatti">Contatti</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul className="flex gap-4">
-                        <li>
-                            <NavLink to="/progetti">Progetti</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about">Chi sono</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contatti">Contatti</NavLink>
-                        </li>
-                    </ul>
-                </nav>
             </section>
 
         </>
