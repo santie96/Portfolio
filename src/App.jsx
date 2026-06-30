f8e3cca4618d7c04d31c6bb4b94cf72bd565ccf4
santie96 <santi96emma@gmail.com>
2026-06-30 18:05:58 +0200

    Struttura progetto

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">
+      Ciao Tailwind!
+    </h1>
+  )
+}
+export default App
---
+++ src/component/layout/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+            <section className=" bg-pink-600 w-full py-30">
+                <p className="text-white font-black">Ciao sono un paragrafo</p>
+            </section>
+            
+        </>
+
+    )
+}
+
+export default Navbar
