f1a36e099c3edb2a1f9594fc21bd1ec2f22ebcc6
santie96 <santi96emma@gmail.com>
2026-07-09 21:32:09 +0200

    fix Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
+import { Routes, Route } from "react-router-dom"
 import Layout from "./components/layout/Layout.jsx"
-import HomePage from "./pages/NavPages/HomePage.jsx";
-import Progetti from "./pages/NavPages/Progetti.jsx";
-import About from "./pages/NavPages/About.jsx"
-import Contatti from "./pages/NavPages/Contatti.jsx"
-import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/NavPages/HomePage.jsx"
+import Progetti from "./components/pages/NavPages/Progetti.jsx"
+import ChiSono from "./components/pages/NavPages/ChiSono.jsx"
+import Contatti from "./components/pages/NavPages/Contatti.jsx"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<Home />} />
         <Route path="progetti" element={<Progetti />} />
-        <Route path="about" element={<About />} />
+        <Route path="chi_sono" element={<ChiSono />} />
         <Route path="contatti" element={<Contatti />} />
       </Route>
     </Routes>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,6 +4,8 @@ import logoPortfolio from "../../assets/img/logo-portolio.png";
 import { FiSun } from "react-icons/fi";
 import { FaBars } from "react-icons/fa6";
 import { useState } from "react";
+import SideBarMenu from "./component_navbar/SideBarMenu";
+
 
 
 
@@ -12,87 +14,42 @@ function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <>
-            <section className="bg-slate-800 w-full flex justify-center h-20 border-b-1 border-slate-200">
-                <div className="flex items-center justify-between text-center w-4/5">
-                    <div className="flex items-center gap-4 basis-1/4">
-                        <img className="w-18" src={logoPortfolio} alt="Logo portfolio" />
-                    </div>
+            <header className="w-full bg-gray-900 flex justify-center items-center">
+                <div className="flex items-center justify-between w-full p-7">
+                    <section className="flex items-center gap-5 font-title">
+                        <NavLink to="/"><img src={`${logoPortfolio}`} alt="logo" className="w-18" /></NavLink>
+                        
+                        <h2 className="max-sm:hidden md:text-2xl text-text-inverse">Santi Andrea Emma</h2>
+                    </section>
 
-                    <nav className="md:flex hidden items-center  basis-2/4">
-                        <ul className="flex gap-4 font-text text-1xl font-medium text-text-primary text-slate-200">
+
+                    <section className="flex items-center">
+                        <button className="cursor-pointer text-text-inverse transition-transform hover:scale-125 duration-300" onClick={() => setIsMenuOpen(valore => !valore)}>
+                            <FaBars className="text-xl"/>
+                        </button>
+
+                        <SideBarMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+                    </section>
+
+
+                    {/* <section>
+                        <ul>
                             <li>
-                                <NavLink className="" to="/progetti">Progetti</NavLink>
+                                <NavLink>Progetto</NavLink>
                             </li>
                             <li>
-                                <NavLink to="/about">Chi sono</NavLink>
+                                <NavLink>Chi sono</NavLink>
                             </li>
                             <li>
-                                <NavLink to="/contatti">Contatti</NavLink>
+                                <NavLink>Contatti</NavLink>
                             </li>
                         </ul>
-                    </nav>
 
-                    <nav className="md:hidden flex">
-                        <FaBars
-                            onClick={() => setIsMenuOpen(prev => !prev)}
-                            className="text-2xl text-slate-200 cursor-pointer z-20"/>
+                    </section> */}
 
-                        <div className={`absolute top-20 right-0 w-60 h-60 bg-slate-700 rounded-b-2xl flex justify-center transition-all duration-300 ease-in-out ${isMenuOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0 pointer-events-none"}`}>
-
-                            <ul className="flex flex-col items-center justify-evenly w-full text-slate-200 font-medium">
-
-                                <li className="w-full h-full">
-                                    <NavLink
-                                        className="hover:bg-slate-600 w-full h-full flex items-center justify-center"
-                                        to="/progetti"
-                                        onClick={() => setIsMenuOpen(false)}
-                                    >
-                                        Progetti
-                                    </NavLink>
-                                </li>
-
-                                <li className="w-full h-full">
-                                    <NavLink
-                                        className="hover:bg-slate-600 w-full h-full flex items-center justify-center"
-                                        to="/about"
-                                        onClick={() => setIsMenuOpen(false)}
-                                    >
-                                        Chi sono
-                                    </NavLink>
-                                </li>
-
-                                <li className="w-full h-full">
-                                    <NavLink
-                                        className="hover:bg-slate-600 w-full h-full flex items-center justify-center"
-                                        to="/contatti"
-                                        onClick={() => setIsMenuOpen(false)}
-                                    >
-                                        Contatti
-                                    </NavLink>
-                                </li>
-
-                                <li className="w-full h-full">
-
-                                    <button className="hover:bg-slate-600 w-full h-full flex items-center justify-center gap-3 group cursor-pointer rounded-b-2xl">
-
-                                        <FiSun className="text-[1.3rem] transition-transform duration-300 group-hover:scale-110" />
-
-                                        <span>Dark Mode</span>
-
-                                    </button>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </nav>
-
-                    <button className=" group text-slate-200 border-3 border-slate-200 py-2 px-6 rounded-4xl cursor-pointer md:flex hidden">
-
-                        <FiSun className="text-[1.3rem] scale-90 transition-transform duration-300 group-hover:scale-100" />
-
-                    </button>
                 </div>
-            </section>
+
+            </header>
 
         </>
 
