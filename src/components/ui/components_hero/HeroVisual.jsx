548690bc5ed8b6aea483b25c85bed96806bec3ba
santie96 <santi96emma@gmail.com>
2026-07-24 19:05:58 +0200

    fix2 HeroVisual

--- src/components/ui/components_hero/HeroVisual.jsx
+++ src/components/ui/components_hero/HeroVisual.jsx
@@ -7,28 +7,28 @@ function HeroVisual() {
       <div className="relative flex items-center justify-center w-full h-96 overflow-hidden font-text mt-10">
         
         <div
-          className="absolute z-39 border-2 border-[#06B6D4]/30 rounded-3xl h-64 w-64 bottom-20 rotate-[70deg]"
+          className="absolute z-39 border-2 border-[#06B6D4]/30 rounded-3xl h-64 w-64 bottom-20 rotate-[70deg] shadow-[0_0_25px_rgba(6,182,212,0.1)]"
           style={{ borderRadius: '60% 30% 100% 40%' }}
         />
 
         <div
-          className="absolute z-40 bg-linear-to-bl from-[#06B6D4]/20 via-[#3B82F6]/20 to-[#7C3AED]/20 rounded-3xl h-64 w-64 bottom-20 rotate-[-20deg]"
+          className="absolute z-40 bg-linear-to-bl from-[#06B6D4]/20 via-[#3B82F6]/20 to-[#7C3AED]/20 rounded-3xl h-64 w-64 bottom-20 rotate-[-20deg] shadow-[0_0_25px_rgba(6,182,212,0.1)]"
           style={{ borderRadius: '60% 30% 100% 40%' }}
         />
 
         <div
-          className="absolute z-41 border-2 border-[#06B6D4]/30 rounded-3xl h-64 w-64 bottom-20 rotate-[-80deg]"
+          className="absolute z-41 border-2 border-[#06B6D4]/30 rounded-3xl h-64 w-64 bottom-20 rotate-[-80deg] shadow-[0_0_25px_rgba(6,182,212,0.1)]"
           style={{ borderRadius: '60% 30% 100% 40%' }}
         />
 
         <div
-          className="absolute z-42 bg-linear-to-br from-[#06B6D4]/40 via-[#3B82F6]/40 to-[#7C3AED]/60 h-64 w-64"
+          className="absolute z-42 bg-linear-to-br from-[#06B6D4]/40 via-[#3B82F6]/40 to-[#7C3AED]/60 h-64 w-64 shadow-[0_0_25px_rgba(6,182,212,0.3)]"
           style={{ borderRadius: '60% 60% 100% 40%' }}
         />
 
         <div className="absolute z-44 left-0 top-40 ">
           <div className="bg-[#101A2E]/60 px-4 py-1 rounded-xl border-2 border-[#06B6D4]/30">
-            <div className="space-x-1">
+            <div className="space-x-1 shadow-[0_0_25px_rgba(6,182,212,0.1)]">
               <span className="inline-block bg-red-600 p-0.5 rounded-full"></span>
               <span className="inline-block bg-yellow-400 p-0.5 rounded-full"></span>
               <span className="inline-block bg-green-500 p-0.5 rounded-full"></span>
@@ -77,14 +77,14 @@ function HeroVisual() {
         </div>
 
         <div className="absolute z-44 bottom-10">
-          <div className="bg-[#101A2E]/60 px-4 py-2 rounded-xl border-2 border-[#06B6D4]/30 flex items-center gap-2 ">
+          <div className="bg-[#101A2E]/60 px-4 py-2 rounded-xl border-2 border-[#06B6D4]/30 flex items-center gap-2 shadow-[0_0_25px_rgba(6,182,212,0.1)]">
             <span className="w-1.5 h-1.5 shrink-0 rounded-full bg-green-500"></span>
             <p className="text-white text-[0.8rem]">Available for new opportunities</p>
           </div>
         </div>
 
         <div className="absolute z-44 right-8 top-13 text-sm">
-          <div className="bg-[#101A2E]/60 p-2 rounded-xl border-2 border-[#06B6D4]/30">
+          <div className="bg-[#101A2E]/60 p-2 rounded-xl border-2 border-[#06B6D4]/30 shadow-[0_0_5px_rgba(6,182,212,0.3)]">
             <span className="text-white">{`<`}</span>
             <span className="text-[#06B6D4]">{`/`}</span>
             <span className="text-white">{`>`}</span>
