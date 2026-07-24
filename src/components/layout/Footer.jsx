c29487891bd42c7895eb7e5ec71517981f65653d
santie96 <santi96emma@gmail.com>
2026-07-24 19:21:06 +0200

    fix

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,23 +4,38 @@ import { NavLink } from "react-router-dom";
 function Footer() {
     return (
         <>
-            <section className=" bg-pink-600 w-full py-10">
-                <IoCodeSlashSharp />
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/progetti">Progetti</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about">Chi sono</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contatti">Contatti</NavLink>
-                        </li>
-                    </ul>
-                </nav>
+            <section className=" bg-[#101A2E] w-full py-5 border-t border-t-[#24344D]">
+                <div className="flex items-center px-10">
+                    {/* Logo - view Mobile {#afd,13} */}
+                        <NavLink className="md:hidden" to="/">
+                            <h2 className="text-xl text-white font-semibold tracking-tighter">
+                                <span className="text-[#3B82F6]">{`<`}</span>
+
+                                S
+
+                                <span className="text-[#3B82F6]">Dev</span>
+
+                                /
+
+                                <span className="text-[#3B82F6]">{`>`}</span>
+                            </h2>
+                        </NavLink>
+                    <nav>
+                        <ul className="flex">
+                            <li>
+                                <NavLink to="/progetti">Progetti</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about">Chi sono</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contatti">Contatti</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </section>
-            
+
         </>
 
     )
